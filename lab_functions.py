c2ccd810fb28784b44c74b336cd2e2a85da7865c
Ashraf-cs <ashraf.gh.cs@gmail.com>
2022-02-15 15:00:34 +0300

    First commit

---
+++ lab_functions.py
@@ -0,0 +1,11 @@
+def func(num):
+    List = list(range(1,num+1))
+    List.reverse()
+    while List:
+        items = ""
+        for item in List:
+            items += f"{item} "
+        print(items)
+        List.pop(0)
+
+func(5)
